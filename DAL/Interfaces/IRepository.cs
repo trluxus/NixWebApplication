09472458c0c583d0fd9f8238b2112010ded59c8f
Dima Moruha <trluxus@outlook.com>
2021-06-25 12:08:28 +0300

    NixWebApplication.
    Add IRepository interface.

---
+++ DAL/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Create(T item);
+        void Delete(int id);
+    }
+}
