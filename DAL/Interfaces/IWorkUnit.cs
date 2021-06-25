9554329c7b8df08c47d3b9c7fb77a806f645f9ce
Dima Moruha <trluxus@outlook.com>
2021-06-25 12:39:09 +0300

    NixWebApplication.
    Add IWorkUnit interface.

---
+++ DAL/Interfaces/IWorkUnit.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IWorkUnit
+    {
+        IRepository<Booking> Bookings { get; }
+        IRepository<Category> Categories { get; }
+        IRepository<Guest> Guests { get; }
+        IRepository<PriceToCategory> PricesToCategories { get; }
+        IRepository<Room> Rooms { get; }
+
+        void Save();
+    }
+}
