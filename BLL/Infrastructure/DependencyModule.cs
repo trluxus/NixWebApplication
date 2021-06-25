852c8274ea0e11bd0b5297bb66f960fe31a1e2b0
Dima Moruha <trluxus@outlook.com>
2021-06-25 14:30:36 +0300

    NixWebApplication.
    Add DependencyModule.

---
+++ BLL/Infrastructure/DependencyModule.cs
@@ -0,0 +1,26 @@
+﻿using DAL.Interfaces;
+using DAL.Repositories;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Infrastructure
+{
+    public class DependencyModule : NinjectModule
+    {
+        private string connectionString;
+
+        public DependencyModule(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public override void Load()
+        {
+            Bind<IWorkUnit>().To<EFWorkUnit>().WithConstructorArgument(connectionString);
+        }
+    }
+}
