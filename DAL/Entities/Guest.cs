fbc9b0aadecf91f5e60b62bfab417d0826d98a85
Dima Moruha <trluxus@outlook.com>
2021-06-25 10:55:01 +0300

    Add project files.

---
+++ DAL/Entities/Guest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Entities
+{
+    class Guest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string Address { get; set; }
+    }
+}
