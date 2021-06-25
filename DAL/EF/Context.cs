7d1099bbe52b401db68de517cdc1f828de371206
Dima Moruha <trluxus@outlook.com>
2021-06-25 12:04:11 +0300

    NixWebApplication.
    Add context class.

---
+++ DAL/EF/Context.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF
+{
+    public class Context : DbContext
+    {
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<PriceToCategory> PricesToCategories { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PriceToCategory>().Property(i => i.Price).HasPrecision(9, 2);
+        }
+    }
+}
