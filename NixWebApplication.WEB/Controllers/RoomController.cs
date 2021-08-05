6f3c1435e1751677c87320fadee6089e9ce31ee3
Dima Moruha <trluxus@outlook.com>
2021-08-05 16:16:36 +0300

    NixWebApplication.
    Added Room controller actions and corresponding views.

--- NixWebApplication.WEB/Controllers/RoomController.cs
+++ NixWebApplication.WEB/Controllers/RoomController.cs
@@ -2,9 +2,12 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using NixWebApplication.BLL.DTO;
 using NixWebApplication.BLL.Interfaces;
 using NixWebApplication.Models;
+using NixWebApplication.Pagination;
+using NixWebApplication.WEB.Models.RoomViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,90 +18,154 @@ namespace NixWebApplication.WEB.Controllers
     [Authorize]
     public class RoomController : Controller
     {
-        private readonly IRoomService _service;
+        private readonly IRoomService _roomService;
+        private readonly ICategoryService _categoryService;
         private readonly IMapper _mapper;
 
-        public RoomController(IRoomService service, IMapper mapper)
+        public RoomController(IRoomService roomService, ICategoryService categoryService, IMapper mapper)
         {
-            this._service = service;
+            this._roomService = roomService;
+            this._categoryService = categoryService;
             this._mapper = mapper;
         }
 
         // GET: RoomController
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder,
+            string searchString,
+            string currentFilter,
+            int? pageNumber)
         {
-            var data = _service.GetAll();
-            return View(_mapper.Map<IEnumerable<RoomDTO>, List<RoomModel>>(data));
+            ViewData["CurrentSort"] = sortOrder;
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+            ViewData["CurrentFilter"] = searchString;
+
+            var data = _roomService.GetAll();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                data = data.Where(s => s.Name.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    data = data.OrderByDescending(s => s.Name);
+                break;
+                default:
+                    data = data.OrderBy(s => s.Name);
+                break;
+            }
+
+            int pageSize = 3;
+            return View(PaginatedList<RoomModel>.
+                Create(_mapper.Map<IEnumerable<RoomDTO>, List<RoomModel>>(data), pageNumber ?? 1, pageSize));
         }
 
         // GET: RoomController/Details/5
         public ActionResult Details(int id)
         {
-            var data = _service.Get(id);
+            var data = _roomService.Get(id);
             return View(_mapper.Map<RoomDTO, RoomModel>(data));
         }
 
         // GET: RoomController/Create
         public ActionResult Create()
         {
+            ViewBag.CategoryId = new SelectList(_categoryService.GetAll(),
+                nameof(CategoryModel.Id), nameof(CategoryModel.Name), null);
             return View();
         }
 
         // POST: RoomController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create([Bind("Id,Name,RoomCategory,IsActive")] RoomModel data)
         {
-            try
+            if (ModelState.IsValid)
             {
+                _roomService.Create(_mapper.Map<RoomModel, RoomDTO>(data));
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            ViewBag.CategoryId = new SelectList(_categoryService.GetAll(),
+                nameof(CategoryModel.Id), nameof(CategoryModel.Name), data.RoomCategory.Id);
+            return View(data);
         }
 
         // GET: RoomController/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var data = _roomService.Get(id.Value);
+
+            if (data == null)
+            {
+                return NotFound();
+            }
+            ViewBag.CategoryId = new SelectList(_categoryService.GetAll(),
+                nameof(CategoryModel.Id), nameof(CategoryModel.Name), data.RoomCategory.Id);
+            return View(_mapper.Map<RoomDTO, RoomModel>(data));
         }
 
         // POST: RoomController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, [Bind("Id,Name,RoomCategory,IsActive")] RoomModel data)
         {
-            try
+            if (id != data.Id)
             {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _roomService.Update(_mapper.Map<RoomModel, RoomDTO>(data));
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            ViewBag.CategoryId = new SelectList(_categoryService.GetAll(),
+                nameof(CategoryModel.Id), nameof(CategoryModel.Name), data.RoomCategory.Id);
+            return View(data);
         }
 
         // GET: RoomController/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var data = _roomService.Get(id.Value);
+
+            if (data == null)
+            {
+                return NotFound();
+            }
+
+            return View(_mapper.Map<RoomDTO, RoomModel>(data));
         }
 
         // POST: RoomController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            _roomService.Delete(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ NixWebApplication.WEB/Models/RoomViewModels/RoomViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NixWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NixWebApplication.WEB.Models.RoomViewModels
+{
+    public class RoomViewModel
+    {
+        public RoomModel Room { get; set; }
+        public IEnumerable<CategoryModel> Categories { get; set; }
+    }
+}
